0bc599cac4f3ef6b53df26f555ffa5e43e4d26c6
nunthanok <nunthanok41@gmail.com>
2020-07-12 15:21:54 +0700

    add vuetify

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require(cors);
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.json({ message: "Welcome to store API" });
